bcabe0162e0b867f69323004425364f1d33a32c8
joao-dias-25 <joaopbdias@gmail.com>
2020-08-17 16:52:27 +0200

    sql lab

---
+++ Labs/module-1_labs/03_mysql/02_lab-intro-sql/lab-2-olist-solutions.sql
@@ -0,0 +1,59 @@
+/* 1 */
+select min(first_contact_date), max(first_contact_date)
+from olist.marketing_qualified_leads;
+
+/* 2 */
+select origin, 
+count(*) as frq
+from olist.marketing_qualified_leads
+where (date(first_contact_date) between '2018-01-01' and'2018-12-31')
+group by origin
+order by frq desc
+limit 3;
+
+/* 3 */
+select first_contact_date, count(mql_id) as mql_entries
+from olist.marketing_qualified_leads
+group by date(first_contact_date)
+order by mql_entries desc
+limit 1;
+
+/* 4 */
+select product_category_name, count(product_category_name)
+from olist.products
+group by product_category_name
+order by count(product_category_name) desc
+limit 2;
+
+/* 5 */
+select product_category_name, product_weight_g
+from olist.products
+order by product_weight_g desc
+limit 1;
+
+/* 6 */
+
+select product_category_name, (product_length_cm+product_height_cm+product_width_cm) as soma
+from olist.products
+order by soma desc
+limit 1;
+
+/* 7 */
+
+select payment_type, count(order_id)
+from olist.order_payments
+group by payment_type
+order by count(order_id) desc
+limit 1;
+
+/* 8 */
+select max(payment_value)
+from olist.order_payments;
+
+/* 9 */
+select seller_state, count(distinct seller_city) as n_cities
+from olist.sellers
+group by seller_state
+order by n_cities desc
+limit 3;
+
---
+++ Labs/module-1_labs/03_mysql/02_lab-intro-sql/solutions.sql
@@ -0,0 +1,56 @@
+/* 1 */
+select  max(price), min(price)
+from olist.order_items;
+
+/* 2 */
+select  min(shipping_limit_date) , max(shipping_limit_date)
+from olist.order_items;
+
+/* 3 */
+SELECT customer_state, count(customer_state) 
+from olist.customers
+group by customer_state
+order by count(customer_state) desc
+limit 3;
+
+/* 4 */
+SELECT customer_city, count(customer_city) 
+from olist.customers
+where customer_state = 'SP'
+group by customer_city
+order by count(customer_city) desc
+limit 3;
+
+/*5 */
+SELECT count(distinct(business_segment))
+from olist.closed_deals;
+
+/*6*/ 
+select business_segment
+from olist.closed_deals
+group by business_segment
+order by sum(declared_monthly_revenue) desc
+limit 3;
+
+/*7*/
+select count(distinct(review_score))
+from olist.order_reviews;
+
+/*8 */
+select *,
+	case
+    when review_score=1 then 'very_bad'
+    when review_score=2 then 'bad'
+    when review_score=3 then 'average'
+    when review_score=4 then 'good'
+    when review_score=5 then 'very_good'
+    end
+from olist.order_reviews
+limit 10;
+
+/*9 */
+select review_score, count(*) as qt
+from olist.order_reviews
+group by review_score
+order by qt desc
+limit 1;
